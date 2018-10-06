36e197d4043c0f61866118481b08590d221b205c
Wuttinunt Chaoyos <wuttinan.wch@gmail.com>
2018-10-06 21:00:31 +0700

    add template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 const app = express()
+
+const template = require('./template')
+
 const port = process.env.PORT || 3000
+
+const slackChannelId = process.env.CHANNEL_ID || ''
+const slackToken = process.env.TOKEN || ''
 
 app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
-    console.log('post work! : ', req.body)
-    res.send('Work!')
-})
+    if (req.body) {
+        
+        const result = template(req.body)
 
-app.get('/webhook', (req, res) => {
-    res.send('Work!')
+        const options = {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json', 'Authorization': 'Bearer xoxp-263257028645-365043984871-450889583637-7c8a620601979f7544ae5388e3c729be' },
+            data: { channel: "GD9MHGK0X", result },
+            url: 'https://slack.com/api/chat.postMessage'
+        }
+
+        axios(options)
+            .then((res) => {
+                console.log('res :', res)
+            })
+            .catch((err) => {
+                console.log('err :', err)
+            })
+
+    }else {
+        res.send('GOT IT!')
+        res.status(200)
+    }
+
 })
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
---
+++ src/template.js
@@ -0,0 +1,49 @@
+
+const template = (data) => {
+    let s
+    if (data){
+        const { status, failed, subject, user, branch, vcs_revision, build_num, build_url, committer_email, committer_name, build_time_millis, start_time, stop_time, previous } = data
+        s =
+        {
+            "attachments": [
+                {
+                    "text": `<${build_url}|#${build_num}> - รายงานสถานะ Circleci`,
+                    "color": `${failed} ? "danger" : "good"`,
+                    "fields": [
+                        {
+                            "title": "Commit",
+                            "value": `<https://github.com/appman-agm/azay-sign-mapper/commit/${vcs_revision}> (${subject})`,
+                            "short": true
+                        },
+                        {
+                            "title": "Status",
+                            "value": `${failed} ? "Fail" : "Success"`,
+                            "short": true
+                        },
+                        {
+                            "title": "Start time",
+                            "value": start_time,
+                            "short": true
+                        },
+                        {
+                            "title": "Stop time",
+                            "value": stop_time,
+                            "short": true
+                        },
+                        {
+                            "title": "Build Time",
+                            "value": `${build_time_millis}` ,
+                            "short": true
+                        }
+                    ],
+                    "footer": `by <https://github.com/${user.login}|${user.name}>`,
+                    "footer_icon": user.avatar_url,
+                    "ts": new Date().getTime()
+                }
+            ]
+        }
+    }
+    return s
+}
+
+module.exports = template
