d0232dd32ea7e5cdaa9d41b0eca85349432d4b5e
Wuttinunt Chaoyos <wuttinan.wch@gmail.com>
2018-10-06 21:34:51 +0700

    [fixed] defect invalid format object

--- src/template.js
+++ src/template.js
@@ -10,40 +10,40 @@ const template = (data) => {
 
         s =
         {
-            "attachments": [
+            attachments: [
                 {
-                    "text": `<${build_url}|#${build_num}> - รายงานสถานะ Circleci`,
-                    "color": `${failed} ? "danger" : "good"`,
-                    "fields": [
+                    text: `<${build_url}|#${build_num}> - รายงานสถานะ Circleci`,
+                    color: `${failed} ? "danger" : "good"`,
+                    fields: [
                         {
-                            "title": "Commit",
-                            "value": `<https://github.com/appman-agm/azay-sign-mapper/commit/${vcs_revision}> (${subject})`,
-                            "short": true
+                            title: "Commit",
+                            value: `<https://github.com/appman-agm/azay-sign-mapper/commit/${vcs_revision}> (${subject})`,
+                            short: true
                         },
                         {
-                            "title": "Status",
-                            "value": `${failed} ? "Fail" : "Success"`,
-                            "short": true
+                            title: "Status",
+                            value: `${failed} ? "Fail" : "Success"`,
+                            short: true
                         },
                         {
-                            "title": "Start time",
-                            "value": start_time,
-                            "short": true
+                            title: "Start time",
+                            value: start_time,
+                            short: true
                         },
                         {
-                            "title": "Stop time",
-                            "value": stop_time,
-                            "short": true
+                            title: "Stop time",
+                            value: stop_time,
+                            short: true
                         },
                         {
-                            "title": "Build Time",
-                            "value": `${build_time_millis}` ,
-                            "short": true
+                            title: "Build Time",
+                            value: `${build_time_millis}` ,
+                            short: true
                         }
                     ],
-                    "footer": `by <https://github.com/${user.login}|${user.name}>`,
-                    "footer_icon": user.avatar_url,
-                    "ts": new Date().getTime()
+                    footer: `by <https://github.com/${user.login}|${user.name}>`,
+                    footer_icon: user.avatar_url,
+                    ts: new Date().getTime()
                 }
             ]
         }
